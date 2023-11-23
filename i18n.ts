80e9375e377110c64eb851afc91770dd6536884d
dunt <mahoagray@gmail.com>
2023-11-23 13:24:10 +0900

    fix: fixed build error

--- i18n.ts
+++ i18n.ts
@@ -1,22 +1,8 @@
-import { cookies } from 'next/headers'
 import { getRequestConfig } from 'next-intl/server'
 
 import Const from './constants/common'
+import { getLocale } from './middleware'
 import { Locale } from './types'
-
-export const getLocale = () => {
-  const cookieStore = cookies()
-  const theme = cookieStore.get('theme')?.value
-  const lang =
-    cookieStore.get(Const.LOCALE_COOKIE_NAME)?.value || Const.DEFAULT_LOCALE
-
-  const isValidLocale = Object.keys(Const.LOCALES).some((cur) => cur === lang)
-
-  const locale = isValidLocale ? lang : Const.DEFAULT_LOCALE
-  const longLocale = Const.LOCALES[locale as Locale]
-
-  return { locale, theme, longLocale }
-}
 
 export const getTranslations = async () => {
   const { locale, theme } = getLocale()
